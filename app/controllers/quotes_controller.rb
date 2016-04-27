2e854f11d6a934458e47045e54337135ddcd07fe
GitCodeRunner <nicolevagt@googlemail.com>
2016-04-27 23:21:10 +0200

    initial commit

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,52 @@
+class QuotesController < ApplicationController
+	before_action :find_quote, only: [:show, :edit, :update, :destroy]
+
+  def index
+  	@quotes = Quote.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+  	@quote = Quote.new
+  end
+
+  def create
+  	@quote = Quote.new(quote_params)
+
+  	if @quote.save
+  		redirect_to @quote
+  	else
+  		render 'new'
+  	end
+  end
+
+  def edit
+  end
+
+  def update
+  	if @quote.update(quote_params)
+  		redirect_to @quote
+  	else
+  		render 'new'
+  	end
+  end
+
+  def destroy
+  	@quote.destroy
+  	redirect_to root_path
+  end
+
+
+  private
+
+  def quote_params
+  	params.require(:quote).permit(:author, :quote, :category_id)
+  end
+
+  def find_quote
+  	@quote = Quote.find(params[:id])
+  end
+
+end
